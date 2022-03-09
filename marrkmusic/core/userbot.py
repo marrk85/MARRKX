e4183cb8bbf40b55584c8de4a40da21703be0b61
marrk85 <96385065+marrk85@users.noreply.github.com>
2022-03-09 18:08:04 +0530

    Update userbot.py

--- marrkmusic/core/userbot.py
+++ marrkmusic/core/userbot.py
@@ -48,7 +48,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("marrkmusic")
             except:
                 pass
             assistants.append(1)
@@ -77,7 +77,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
+                await self.two.join_chat("marrkmusic")
             except:
                 pass
             assistants.append(2)
@@ -106,7 +106,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
+                await self.three.join_chat("marrkmusic")
             except:
                 pass
             assistants.append(3)
@@ -135,7 +135,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
+                await self.four.join_chat("marrkmusic")
             except:
                 pass
             assistants.append(4)
@@ -164,7 +164,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
+                await self.five.join_chat("marrkmusic")
             except:
                 pass
             assistants.append(5)
